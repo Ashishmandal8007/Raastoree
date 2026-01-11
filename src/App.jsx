ec40fc47466714f0a66057d2cae66ca34c156f82
Ashish Mandal <ashishmandal8007@gmail.com>
2026-01-11 17:33:42 +0530

    Fix css import case issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './app.css';
+import './App.css';
 const RestaurantWebsite = () => {
   const [activeSection, setActiveSection] = useState('home');
   const [activeMenuCategory, setActiveMenuCategory] = useState('food');
